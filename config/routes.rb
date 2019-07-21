be8e2a37a5eda92e0de584cd1762715e5fb237b2
Mosaab Elhassan <mosaabe35@gmail.com>
2019-07-21 14:41:21 -0400

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
-Rails.application.routes.draw do
-  resources :portfolios
+Rails.application.routes.draw do  
+  resources :blogs
+  resources :portfolios, except: [:show]
+
+# to: for controller action, as: to specify prefix to to be used for path and url
+# the get to map a path to an action and view
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   
   get 'about', to: 'pages#about'
   get 'contactus', to: 'pages#contact'
 
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to: 'pages#home' #get 'pages/home' localhost will ditect to home page
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
